94be34d4e59e525f76fadcfa4d3c3dbce6209ffd
LeeYoungwoo <leeduddn98@gmail.com>
2025-09-08 11:24:37 +0900

    [update] 패키지명 dabojob으로 변경

--- services/spring-server/src/main/java/com/dabojob/Application.java
+++ services/spring-server/src/main/java/com/dabojob/Application.java
@@ -1,4 +1,4 @@
-package com.dabojab;
+package com.dabojob;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- services/spring-server/src/test/java/com/dabojob/ApplicationTests.java
+++ services/spring-server/src/test/java/com/dabojob/ApplicationTests.java
@@ -1,4 +1,4 @@
-package com.dabojab;
+package com.dabojob;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
