acd59d3fa0c06cc2f106e8983d3641a399d75221
easy-min <minisolifewow@sookmyung.ac.kr>
2025-09-09 17:34:55 +0900

    :bug: [BE/bug] '__init__.py'에서 참조 'redis_client'을(를) 찾을 수 없습니다  오류  수정 사항:   1. common/__init__.py에 명시적 import 추가   2. 모든 requirements.txt에 python-dotenv==1.0.0 추가

--- backend/private-server/common/__init__.py
+++ backend/private-server/common/__init__.py
@@ -1,1 +1,6 @@
 # Common modules for private servers
+from .redis_client import RedisStreamClient
+from .file_manager import FileManager
+from .config import *
+
+__all__ = ['RedisStreamClient', 'FileManager']
