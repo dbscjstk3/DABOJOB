daf99c8755eeca8ac1cca2d13f19257152a319b3
0224LJH <kavae550@gmail.com>
2025-09-11 16:28:35 +0900

    :sparkles: [feat] JobPosting 및 DartJob 엔티티 생성

---
+++ backend/spring-server/src/main/java/com/dabojob/jobposting/entity/DartJob.java
@@ -0,0 +1,46 @@
+package com.dabojob.jobposting.entity;
+
+import com.dabojob.company.entity.DartCompany;
+import com.dabojob.global.entity.BaseTimeEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Table(name = "dart_jobs")
+public class DartJob extends BaseTimeEntity {
+
+    @Id
+    @Column(name="dart_job_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long dartJobId;
+
+    @OneToOne
+    @JoinColumn(name="job_id")
+    private JobPosting jobPosting;
+
+    @ManyToOne(fetch =  FetchType.LAZY)
+    @JoinColumn(name="dart_id")
+    private DartCompany  dartCompany;
+
+    @Column(name="company_name_normalized")
+    private String companyNameNormalized;
+
+}
