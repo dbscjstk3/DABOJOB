9e933142a69b5449d0191ea73cb0c153afc0c7c9
0224LJH <kavae550@gmail.com>
2025-09-11 16:05:21 +0900

    :sparkles: [feat] CompanyAnalysisSummary와 SummaryStatus 생성
    CompanyAnalysisSummary와, 이 Summary의 상태를 나타내는 Enum인 SummaryStatus 생성

---
+++ backend/spring-server/src/main/java/com/dabojob/analysis/entity/CompanyAnalysisSummary.java
@@ -0,0 +1,57 @@
+package com.dabojob.analysis.entity;
+
+import com.dabojob.company.entity.DartCompany;
+import com.dabojob.global.entity.BaseTimeEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.Lob;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Table(name = "company_analysis_summaries")
+public class CompanyAnalysisSummary extends BaseTimeEntity {
+
+    @Id
+    @Column(name="summary_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long summaryId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "dart_id")
+    private DartCompany dartCompany;
+
+    @Column(name="company_name_normalized")
+    private String companyNameNormalized;
+
+    @Lob
+    @Column(name="full_summary",columnDefinition = "TEXT")
+    private String fullSummary;
+
+    @Enumerated(EnumType.STRING)  // DB에 "CREATED", "UPDATED", "FINISHED" 저장
+    @Column(name = "status", nullable = false)
+    private SummaryStatus status;
+
+
+
+
+
+}
