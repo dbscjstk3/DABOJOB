862dc9eb75ad0ac616aa2883a82a7a1c011d4f19
jayeunpark <jayeunpark0704@gmail.com>
2025-09-12 14:41:25 +0900

    [FE] chore: eslint에 ts 규칙 추가

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -20,6 +20,16 @@ export default tseslint.config([
       ecmaVersion: 2020,
       globals: globals.browser,
     },
+    rules: {
+      '@typescript-eslint/no-unused-vars': [
+        'error',
+        {
+          argsIgnorePattern: '^_',
+          varsIgnorePattern: '^_',
+          caughtErrorsIgnorePattern: '^_',
+        },
+      ],
+    },
   },
   eslintConfigPrettier,
 ]);
