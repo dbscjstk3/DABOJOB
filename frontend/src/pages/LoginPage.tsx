609443c3f95140fb362c211c7a722e9c4df0c18a
dbscjstk3 <dbscjstk3@gmail.com>
2025-09-17 13:51:00 +0900

    fix : 로그인 페이지 구조 변경으로 라우터 설정 업데이트

--- frontend/src/pages/LoginPage.tsx
+++ frontend/src/pages/LoginPage.tsx
@@ -1,5 +1,7 @@
 import { Button } from '../components/common/atoms/Button';
 import { Typography } from '../components/common/atoms/Typography';
+import { LoginVisual } from '../components/common/molecules/LoginVisual';
+import { BackgroundDecorations } from '../components/common/molecules/BackgroundDecorations';
 import { API_ENDPOINTS } from '../lib/api';
 import DABOJOB_logo from '../assets/logo/DABOJOB_logo.svg';
 import SSAFY_logo from '../assets/logo/ssafyLogo.png';
