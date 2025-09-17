8a4bd062cdca35c3316b95c6f41808acd0af41db
0224LJH <kavae550@gmail.com>
2025-09-17 14:17:20 +0900

    [feat] SummaryController에 API 엔드포인트 추가
    특정 회사의 가장 최근에 생성된 보고서를 반환하는 엔드포인트 구현

--- backend/spring-server/src/main/java/com/dabojob/summary/repository/CompanyAnalysisSummaryRepository.java
+++ backend/spring-server/src/main/java/com/dabojob/summary/repository/CompanyAnalysisSummaryRepository.java
@@ -1,9 +1,11 @@
 package com.dabojob.summary.repository;
 
 import com.dabojob.summary.entity.CompanyAnalysisSummary;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface CompanyAnalysisSummaryRepository extends JpaRepository<CompanyAnalysisSummary, Long> {
+    Optional<CompanyAnalysisSummary> findFirstByCompany_CompanyIdOrderByCreatedAtDesc(Long companyCompanyId);
 }
