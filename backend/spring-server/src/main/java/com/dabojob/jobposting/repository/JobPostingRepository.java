8188bf70fd9686354b2869d628aaea9a126fad3a
0224LJH <kavae550@gmail.com>
2025-09-18 12:56:55 +0900

    [feat] JobPostingRepository 최적화
    @EntityGraph를 통해 조회 최적화

--- backend/spring-server/src/main/java/com/dabojob/jobposting/repository/JobPostingRepository.java
+++ backend/spring-server/src/main/java/com/dabojob/jobposting/repository/JobPostingRepository.java
@@ -3,8 +3,10 @@ package com.dabojob.jobposting.repository;
 import com.dabojob.jobposting.entity.JobPosting;
 import java.time.LocalDate;
 import java.util.List;
+import java.util.Optional;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -13,11 +15,17 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface JobPostingRepository extends JpaRepository<JobPosting,Long> {
 
+    @Override
+    @EntityGraph(attributePaths = {"company", "jobSector"})
+    Optional<JobPosting> findById(Long id);
+
+    @EntityGraph(attributePaths = {"company", "jobSector"})
     Page<JobPosting> findByCompanyId(@Param("companyId") Long companyId, Pageable pageable);
 
     @Query("SELECT jp FROM JobPosting jp " +
             "WHERE (jp.postingDate <= :endDate) AND " +
             "(jp.deadlineDate >= :startDate )")
+    @EntityGraph(attributePaths = {"company", "jobSector"})
     List<JobPosting> findByDateRange(@Param("startDate") LocalDate startDate,
                                      @Param("endDate") LocalDate endDate);
 }
